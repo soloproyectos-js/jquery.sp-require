444f973da6f4783704fd2b369795b067807fa870
Gonzalo Chumillas <gchumillas@email.com>
2015-11-18 21:52:04 +0100

    update dist

--- make-dist.sh
+++ make-dist.sh
@@ -9,11 +9,11 @@ rm -rf dist/*
 cat \
     src/jquery.sp-require-config.js \
     src/jquery.sp-require-loader.js \
-    src/jquery.sp-require-url-loader.js \
+    src/jquery.sp-require-cache-loader.js \
     src/jquery.sp-require-library.js \
     src/jquery.sp-require.js \
 | uglifyjs \
     --compress \
     --mangle \
-    --preamble "/*! jQuery.spRequire v0.2.0 | Copyright (c) 2015 Gonzalo Chumillas | https://github.com/soloproyectos-js/jquery.sp-require/blob/master/LICENSE */" \
+    --preamble "/*! jQuery.spRequire v0.3.0 | Copyright (c) 2015 Gonzalo Chumillas | https://github.com/soloproyectos-js/jquery.sp-require/blob/master/LICENSE */" \
     -o dist/jquery.sp-require.min.js
